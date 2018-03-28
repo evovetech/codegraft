e0cd5eee606789a09b203606484c93c0ac6a0942
Layne Penney <layne@evove.tech>
2018-03-28 11:18:19 -0500

    initial commit

---
+++ sample/src/main/java/tech/evove/sample/App.kt
@@ -0,0 +1,33 @@
+/*
+ * Copyright 2018 evove.tech
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package tech.evove.sample
+
+import android.app.Application
+import io.realm.Realm
+import io.realm.RealmConfiguration
+
+class App : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        Realm.init(this)
+        val config = RealmConfiguration.Builder()
+                .name("app.realm")
+                .schemaVersion(1)
+                .build()
+        Realm.setDefaultConfiguration(config)
+    }
+}
---
+++ sample/src/main/java/tech/evove/sample/MainActivity.kt
@@ -0,0 +1,54 @@
+/*
+ * Copyright 2018 evove.tech
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package tech.evove.sample
+
+import android.os.Bundle
+import android.support.design.widget.BottomNavigationView
+import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
+        when (item.itemId) {
+            R.id.navigation_home -> {
+                message.setText(R.string.title_home)
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.navigation_dashboard -> {
+                val user = User.getOrCreate("laynepenney@gmail.com") {
+                    firstName = "Layne"
+                    lastName = "Penney"
+                }
+                val text = getString(R.string.title_dashboard) + "\n\nUser: " + user.toString()
+                message.text = text
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.navigation_notifications -> {
+                message.setText(R.string.title_notifications)
+                return@OnNavigationItemSelectedListener true
+            }
+        }
+        false
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
+    }
+}
