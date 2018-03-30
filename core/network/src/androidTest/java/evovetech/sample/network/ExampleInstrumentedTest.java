bb09c13dc312be243d849159e98ff8d3ac5e1202
Layne Penney <layne@evove.tech>
2018-03-29 19:21:52 -0500

    minor package updates

--- core/network/src/androidTest/java/evovetech/sample/network/ExampleInstrumentedTest.java
+++ core/network/src/androidTest/java/evovetech/sample/network/ExampleInstrumentedTest.java
@@ -14,22 +14,31 @@
  * limitations under the License.
  */
 
-package evovetech.sourcerer.sample.annotations;
+package evovetech.sample.network;
+
+import android.content.Context;
+import android.support.test.InstrumentationRegistry;
+import android.support.test.runner.AndroidJUnit4;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
 
 import static org.junit.Assert.*;
 
 /**
- * Example local unit test, which will execute on the development machine (host).
+ * Instrumented test, which will execute on an Android device.
  *
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
+@RunWith(AndroidJUnit4.class)
 public
-class ExampleUnitTest {
+class ExampleInstrumentedTest {
     @Test
     public
-    void addition_isCorrect() {
-        assertEquals(4, 2 + 2);
+    void useAppContext() {
+        // Context of the app under test.
+        Context appContext = InstrumentationRegistry.getTargetContext();
+
+        assertEquals("evovetech.sample.network.test", appContext.getPackageName());
     }
 }
--- core/network/src/test/java/evovetech/sample/network/ExampleUnitTest.java
+++ core/network/src/test/java/evovetech/sample/network/ExampleUnitTest.java
@@ -14,31 +14,22 @@
  * limitations under the License.
  */
 
-package evovetech.sourcerer.sample.network;
-
-import android.content.Context;
-import android.support.test.InstrumentationRegistry;
-import android.support.test.runner.AndroidJUnit4;
+package evovetech.sample.network;
 
 import org.junit.Test;
-import org.junit.runner.RunWith;
 
 import static org.junit.Assert.*;
 
 /**
- * Instrumented test, which will execute on an Android device.
+ * Example local unit test, which will execute on the development machine (host).
  *
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
-@RunWith(AndroidJUnit4.class)
 public
-class ExampleInstrumentedTest {
+class ExampleUnitTest {
     @Test
     public
-    void useAppContext() {
-        // Context of the app under test.
-        Context appContext = InstrumentationRegistry.getTargetContext();
-
-        assertEquals("evovetech.sourcerer.sample.network.test", appContext.getPackageName());
+    void addition_isCorrect() {
+        assertEquals(4, 2 + 2);
     }
 }
