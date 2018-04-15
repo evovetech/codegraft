0c476d7f01196f9ff08025e3cb0cbe26a3a01b53
Layne Penney <layne@evove.tech>
2018-04-15 13:14:11 -0500

    git subrepo pull sourcerer
    subrepo:
      subdir:   "sourcerer"
      merged:   "607ae5c"
    upstream:
      origin:   "git@bitbucket.org:evovetech/sourcerer.git"
      branch:   "master"
      commit:   "607ae5c"
    git-subrepo:
      version:  "0.3.1"
      origin:   "git@github.com:evovetech/git-subrepo.git"
      commit:   "a7ee886"

--- sourcerer/metalava/stub-annotations/src/main/java/android/support/annotation/Migrate.java
+++ sourcerer/metalava/stub-annotations/src/main/java/android/support/annotation/Migrate.java
@@ -1,11 +1,11 @@
 /*
- * Copyright 2018 evove.tech
+ * Copyright (C) 2017 The Android Open Source Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ *      http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -13,18 +13,17 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+package android.support.annotation;
 
-pluginManagement {
-    apply from: 'gradle/settings.gradle'
-    app.configurePlugins(it)
+import static java.lang.annotation.ElementType.ANNOTATION_TYPE;
+import static java.lang.annotation.RetentionPolicy.CLASS;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+@Documented
+@Retention(CLASS)
+@Target({ANNOTATION_TYPE})
+public @interface Migrate {
 }
-
-include(
-        ':io',
-        ':annotations',
-        ':model',
-        ':app-processor',
-        ':lib-processor',
-        ':inject',
-        ':metalava'
-)
