e067ebc715a66c4480506ecdacec21e832672dba
Layne Penney <layne@evove.tech>
2018-05-05 21:01:42 -0500

    updates

--- buildSrc/src/main/java/evovetech/gradle/plugin/ByteBuddyTransform.kt
+++ buildSrc/src/main/java/evovetech/gradle/plugin/ByteBuddyTransform.kt
@@ -19,6 +19,8 @@ package evovetech.gradle.plugin
 import com.android.build.api.transform.DirectoryInput
 import com.android.build.api.transform.Format
 import com.android.build.api.transform.Format.DIRECTORY
+import com.android.build.api.transform.Format.JAR
+import com.android.build.api.transform.JarInput
 import com.android.build.api.transform.QualifiedContent
 import com.android.build.api.transform.QualifiedContent.ContentType
 import com.android.build.api.transform.QualifiedContent.DefaultContentType.CLASSES
@@ -28,9 +30,14 @@ import com.android.build.api.transform.QualifiedContent.Scope.EXTERNAL_LIBRARIES
 import com.android.build.api.transform.QualifiedContent.Scope.PROJECT
 import com.android.build.api.transform.QualifiedContent.Scope.SUB_PROJECTS
 import com.android.build.api.transform.Transform
+import com.android.build.api.transform.TransformInput
 import com.android.build.api.transform.TransformInvocation
 import com.android.utils.FileUtils
+import net.bytebuddy.ClassFileVersion
+import net.bytebuddy.dynamic.ClassFileLocator
+import net.bytebuddy.pool.TypePool
 import java.io.File
+import java.util.jar.JarFile
 
 class ByteBuddyTransform : Transform() {
     override
@@ -53,7 +60,7 @@ class ByteBuddyTransform : Transform() {
 
     override
     fun isIncremental(): Boolean {
-        return false
+        return true
     }
 
     override
@@ -66,91 +73,230 @@ class ByteBuddyTransform : Transform() {
     }
 
     override
-    fun transform(transformInvocation: TransformInvocation) {
-        transformInvocation.run()
-    }
-
-    private
-    fun TransformInvocation.runner(format: Format): (content: QualifiedContent) -> Unit = {
-        val outFile = outputProvider.getContentLocation(it.name, it.contentTypes, it.scopes, format)
-        FileTransform(it, outFile, format).run()
-    }
-
-    fun TransformInvocation.run() {
-        directoryInputs.forEach(runner(Format.DIRECTORY))
-        jarInputs.forEach(runner(Format.JAR))
-    }
+    fun transform(transformInvocation: TransformInvocation) = Runner(transformInvocation)
+            .run()
 }
+
+val Collection<TransformInput>.directoryInputs
+    get() = flatMap { it.directoryInputs }
+
+val Collection<TransformInput>.jarInputs
+    get() = flatMap { it.jarInputs }
 
 val TransformInvocation.directoryInputs
-    get() = inputs.flatMap { it.directoryInputs }
-
-fun DirectoryInput.files() = file.listFiles()
-        .toList()
+    get() = inputs.directoryInputs
 
 val TransformInvocation.jarInputs
-    get() = inputs.flatMap { it.jarInputs }
+    get() = inputs.jarInputs
 
-class FileTransform(
-    val content: QualifiedContent,
-    val out: File,
-    val format: Format = DIRECTORY
-) {
-    init {
-        if (format == DIRECTORY) {
-            out.apply {
-                if (exists()) {
-                    FileUtils.deleteDirectoryContents(this)
-                }
-                mkdirs()
-            }
-        } else {
-            FileUtils.deleteIfExists(out)
-        }
-        println("src=${content.file}, dest=$out")
-    }
-
-    fun run() {
-        if (format == DIRECTORY) {
-            content.file.listFiles()
-                    .forEach(this::write)
-        } else {
-            FileUtils.copyFile(content.file, out)
-        }
-    }
-
-    private
-    fun dest(src: File, dir: File? = null): File {
-        val dest = File(dir ?: out, src.name)
-//        println("src=$src, dest=$dest")
-        return dest
-    }
-
-    private
-    fun File.writeFile(dest: File) {
-        FileUtils.copyFile(this, dest)
-    }
-
-    private
-    fun File.writeDir(dest: File) {
-        dest.mkdirs()
-        listFiles().forEach {
-            it.write(dest)
-        }
-    }
-
-    private
-    fun File.write(dir: File? = null) {
-        val dest = dest(this, dir)
-        if (isDirectory) {
-            writeDir(dest)
-        } else {
-            writeFile(dest)
-        }
-    }
-
-    private
-    fun write(file: File) {
-        file.write()
+fun TransformInvocation.transformer(
+    format: Format
+): (content: QualifiedContent) -> RootTransform = {
+    if (format == DIRECTORY) {
+        RootDirTransform(this, it as DirectoryInput)
+    } else {
+        val outFile = outputProvider.getContentLocation(it.name, it.contentTypes, it.scopes, format)
+        RootJarTransform(it as JarInput, outFile)
     }
 }
+
+class RootDirTransform(
+    invocation: TransformInvocation,
+    override val src: DirectoryInput
+) : RootTransform() {
+    override
+    val dest: File by lazy {
+        src.let {
+            invocation.outputProvider.getContentLocation(it.name, it.contentTypes, it.scopes, format)
+        }.apply {
+            FileUtils.deleteRecursivelyIfExists(this)
+            FileUtils.deleteIfExists(this)
+            mkdirs()
+        }
+    }
+    override
+    val format = Format.DIRECTORY
+    val children by lazy {
+        src.file.listFiles().map { src ->
+            val dest = File(src.name)
+            if (src.isDirectory) {
+                DirTransform(this, src, dest)
+            } else {
+                FileTransform(this, src, dest)
+            }
+        }
+    }
+    val dirs by lazy {
+        children.filterIsInstance<DirTransform>()
+    }
+    val files by lazy {
+        children.filterIsInstance<FileTransform>()
+    }
+    override
+    val all by lazy {
+        files + dirs.flatMap {
+            it.dirs + it
+        }.flatMap {
+            it.files
+        }
+    }
+
+    override
+    fun write(child: BaseTransform) {
+        val src = child.src
+        val dest = File(this.dest, child.dest.path)
+        dest.parentFile.mkdirs()
+        println("$src -> $dest")
+        FileUtils.copyFile(src, dest)
+    }
+}
+
+class RootJarTransform(
+    override val src: JarInput,
+    override val dest: File
+) : RootTransform() {
+    override
+    val format = Format.JAR
+
+    override
+    val all = setOf(
+        FileTransform(this, src.file, dest)
+    )
+
+    override
+    fun write(child: BaseTransform) {
+        val src = child.src
+        val dest = child.dest
+        println("$src -> $dest")
+        FileUtils.copyFile(src, dest)
+    }
+
+    init {
+        FileUtils.deleteRecursivelyIfExists(dest)
+        FileUtils.deleteIfExists(dest)
+    }
+}
+
+sealed
+class BaseTransform {
+    abstract
+    val root: RootTransform
+    abstract
+    val src: File
+    abstract
+    val dest: File
+}
+
+class DirTransform(
+    override val root: RootTransform,
+    override val src: File,
+    override val dest: File
+) : BaseTransform() {
+    val children by lazy {
+        src.listFiles().map { src ->
+            val dest = File(dest, src.name)
+            if (src.isDirectory) {
+                DirTransform(root, src, dest)
+            } else {
+                FileTransform(root, src, dest)
+            }
+        }
+    }
+    val dirs by lazy {
+        children.filterIsInstance<DirTransform>()
+    }
+    val files by lazy {
+        children.filterIsInstance<FileTransform>()
+    }
+    val all by lazy {
+        files + dirs.flatMap {
+            it.dirs + it
+        }.flatMap {
+            it.files
+        }
+    }
+}
+
+class FileTransform(
+    override val root: RootTransform,
+    override val src: File,
+    override val dest: File
+) : BaseTransform()
+
+sealed
+class RootTransform {
+    abstract
+    val src: QualifiedContent
+
+    abstract
+    val dest: File
+
+    abstract
+    val format: Format
+
+    abstract
+    val all: Collection<FileTransform>
+
+    val classFileLocator: ClassFileLocator by lazy {
+        format.let {
+            if (it == DIRECTORY) {
+                ClassFileLocator.ForFolder(src.file)
+            } else {
+                val jarFile = JarFile(src.file)
+                ClassFileLocator.ForJarFile(jarFile)
+            }
+        }
+    }
+
+    abstract
+    fun write(child: BaseTransform)
+}
+
+class Runner(
+    delegate: TransformInvocation
+) : TransformInvocation by delegate {
+    val temp = context.temporaryDir.apply {
+        println("temp=$this")
+        mkdirs()
+    }
+    val dirTransformer = transformer(DIRECTORY)
+    val jarTransformer = transformer(JAR)
+    val ref = referencedInputs.run {
+        directoryInputs.map(dirTransformer) + jarInputs.map(jarTransformer)
+    }
+    val primary = inputs.run {
+        directoryInputs.map(dirTransformer) + jarInputs.map(jarTransformer)
+    }
+    val classFileLocator = (ref + primary).map {
+        it.classFileLocator
+    }.let {
+        ClassFileLocator.Compound(it)
+    }
+    val typePool: TypePool = TypePool.Default.WithLazyResolution(
+        TypePool.CacheProvider.Simple(),
+        classFileLocator,
+        TypePool.Default.ReaderMode.FAST,
+        TypePool.ClassLoading.ofBootPath()
+    )
+    val classFileVersion = ClassFileVersion.JAVA_V7
+
+    fun run() {
+        primary.forEach {
+            val root = it
+            it.all.forEach { root.write(it) }
+        }
+    }
+}
+
+val File.dirs
+    get() = if (isDirectory) {
+        emptyList()
+    } else {
+        listFiles().filter { it.isDirectory }
+    }
+val File.files
+    get() = if (isDirectory) {
+        listFiles().filterNot { it.isDirectory }
+    } else {
+        listOf(this)
+    }
