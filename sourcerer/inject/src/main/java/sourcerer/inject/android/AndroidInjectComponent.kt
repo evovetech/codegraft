bd423bb8a622ae469bcc2bec842a6cca11f21af7
Layne Penney <layne@evove.tech>
2018-07-04 10:43:00 -0500

    prepare for tweak

--- sourcerer/inject/src/main/java/sourcerer/inject/android/AndroidInjectComponent.kt
+++ sourcerer/inject/src/main/java/sourcerer/inject/android/AndroidInjectComponent.kt
@@ -22,74 +22,33 @@ import android.app.Service
 import android.content.BroadcastReceiver
 import android.content.ContentProvider
 import dagger.android.AndroidInjector
-import sourcerer.inject.BootstrapComponent
 
-@BootstrapComponent(
-    applicationModules = [AndroidInjectApplicationModule::class],
-    autoInclude = true,
-    flatten = true
-)
 interface HasApplicationInjector {
     val applicationInjector: AndroidInjector<AndroidApplication>
 }
 
-@BootstrapComponent(
-    applicationModules = [AndroidInjectActivityModule::class],
-    // TODO
-    autoInclude = true,
-    flatten = true
-)
 interface HasActivityInjector {
     val activityInjector: AndroidInjector<Activity>
 }
 
-@BootstrapComponent(
-    applicationModules = [AndroidInjectFragmentModule::class],
-    autoInclude = false,
-    flatten = true
-)
 interface HasFragmentInjector {
     val fragmentInjector: AndroidInjector<Fragment>
 }
 
 typealias SupportFragment = android.support.v4.app.Fragment
 
-@BootstrapComponent(
-    applicationModules = [AndroidInjectSupportFragmentModule::class],
-    autoInclude = false,
-    flatten = true
-)
 interface HasSupportFragmentInjector {
     val supportFragmentInjector: AndroidInjector<SupportFragment>
 }
 
-@BootstrapComponent(
-    applicationModules = [AndroidInjectServiceModule::class],
-    // TODO
-    autoInclude = true,
-    flatten = true
-)
 interface HasServiceInjector {
     val serviceInjector: AndroidInjector<Service>
 }
 
-@BootstrapComponent(
-    applicationModules = [AndroidInjectBroadcastReceiverModule::class],
-    // TODO
-    autoInclude = true,
-    flatten = true
-)
 interface HasBroadcastReceiverInjector {
     val broadcastReceiverInjector: AndroidInjector<BroadcastReceiver>
 }
 
-@BootstrapComponent(
-    applicationModules = [AndroidInjectContentProviderModule::class],
-    // TODO
-    autoInclude = true,
-    flatten = true
-)
 interface HasContentProviderInjector {
     val contentProviderInjector: AndroidInjector<ContentProvider>
 }
-
